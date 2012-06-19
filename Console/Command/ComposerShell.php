8776ef1f2ddd41b6ee9b840151fe6f178e1e0272
U-Zyn Chua <chua@uzyn.com>
2012-06-19 17:46:11 +0800

    Check if Composer is installed at __construct() and allows auto download

--- Console/Command/ComposerShell.php
+++ Console/Command/ComposerShell.php
@@ -7,6 +7,23 @@ class ComposerShell extends AppShell {
 			if (Configure::read('Composer.phar_dir') !== null) $this->pharDir = Configure::read('Composer.phar_dir');
 			else $this->pharDir = dirname(dirname(dirname(__FILE__))).DS.'Vendor'.DS.'Composer'.DS;
 		}
+		
+		/**
+		 * Check if composer.phar is available
+		 */
+		$version = @exec("php {$this->pharDir}composer.phar --version");
+		
+		if (stripos($version, 'Composer') === false || stripos($version, 'version') === false){
+			$this->out('<warning>Composer is not installed.</warning>');
+			$setup = $this->in('Would you like Composer to be set up automatically?', array('y', 'n'), 'y');
+			
+			if ($setup != 'y'){
+				$this->error("Terminating. You may overwrite the location of composer.phar by defining 'Composer.phar_dir' configuration.");
+			}
+			else{
+				$this->setup();
+			}
+		}
 	}
 	
 	public function main() {
@@ -26,7 +43,7 @@ class ComposerShell extends AppShell {
 		}
 		
 		$this->out('<info>Setting up Composer</info>');
-		$this->out("Downloading from $pharURL...");
+		$this->out("Downloading composer.phar from $pharURL...");
 		
 		$content = file_get_contents($pharURL);
 		if ($content === false){
@@ -39,6 +56,6 @@ class ComposerShell extends AppShell {
 			$this->error("Unable to save to {$this->pharDir}composer.phar.");
 		}
 		
-		$this->out("Composer.phar installed and saved successfully.");
+		$this->out("<info>Composer installed and saved successfully.</info>");
 	}
 }
