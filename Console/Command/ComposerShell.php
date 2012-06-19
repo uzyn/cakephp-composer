6b61c72d75a6636924487f15a3291278a7ea635e
U-Zyn Chua <chua@uzyn.com>
2012-06-20 00:37:05 +0800

    Comments

--- Console/Command/ComposerShell.php
+++ Console/Command/ComposerShell.php
@@ -4,7 +4,8 @@ class ComposerShell extends AppShell {
 	
 	/**
 	 * Initialize ComposerShell
-	 * also checks if Composer is installed and offer auto installation option.
+	 * + checks if Composer is installed and offer auto installation option.
+	 * + checks and initialize composer.json
 	 */
 	public function initialize() {
 		if (empty($this->pharDir)) {
@@ -16,10 +17,16 @@ class ComposerShell extends AppShell {
 		$this->_checkComposerJSON();
 	}
 	
+	/**
+	 * Welcome message
+	 */
 	public function startup() {
 		$this->out("<info>Composer plugin for CakePHP</info> by U-Zyn Chua.", 2);
 	}
 	
+	/**
+	 * Catch-all for Composer commands
+	 */
 	public function main() {
 		passthru("php {$this->pharDir}composer.phar ".implode(" ", $this->args)." ".self::_optionsToString($this->params));
 	}
@@ -28,7 +35,7 @@ class ComposerShell extends AppShell {
 	 * Grabs the latest composer.phar from http://getcomposer.org/composer.phar
 	 * Changeable at CakePHP configuration: Composer.phar_url
 	 */
-	public function setup() {
+	private function _setup() {
 		$pharURL = 'http://getcomposer.org/composer.phar';
 		if (Configure::read('Composer.phar_url') !== null) $pharURL = Configure::read('Composer.phar_url');
 		
@@ -53,13 +60,13 @@ class ComposerShell extends AppShell {
 		$this->out("<info>Composer installed and saved successfully.</info>");
 	}
 	
+	/**
+	 * Add options from Composer
+	 * or CakePHP's Shell will exit upon unrecognized options.
+	 */
 	public function getOptionParser() {
 		$parser = parent::getOptionParser();
 		
-		/**
-		 * Listing options from Composer
-		 * or CakePHP's Shell will exit upon unrecognized options.
-		 */
 		$parser->addOptions(array(
 			'help' => array('short' => 'h'),
 			'quiet' => array('short' => 'q'),
@@ -101,13 +108,13 @@ class ComposerShell extends AppShell {
 		
 		if (stripos($version, 'Composer') === false || stripos($version, 'version') === false) {
 			$this->out('<warning>Composer is not installed.</warning>');
-			$setup = $this->in('Would you like Composer to be set up automatically?', array('y', 'n'), 'y');
+			$setup = $this->in('Would you like to install the latest version of Composer?', array('y', 'n'), 'y');
 			
 			if ($setup != 'y') {
 				$this->error("Terminating. You may overwrite the location of composer.phar by defining 'Composer.phar_dir' configuration.");
 			}
 			else {
-				$this->setup();
+				$this->_setup();
 			}
 		}
 	}
