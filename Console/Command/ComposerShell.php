0a337a6522a1a6507489713f381ebefae3ff330f
U-Zyn Chua <chua@uzyn.com>
2012-06-19 16:48:00 +0800

    Set up composer env by downloading the latest composer.phar

--- Console/Command/ComposerShell.php
+++ Console/Command/ComposerShell.php
@@ -5,7 +5,7 @@ class ComposerShell extends AppShell {
 	public function initialize(){
 		if (empty($this->pharDir)){
 			if (Configure::read('Composer.phar_dir') !== null) $this->pharDir = Configure::read('Composer.phar_dir');
-			else $this->pharDir = dirname(dirname(dirname(__FILE__))).'Vendor'.DS.'Composer'.DS;
+			else $this->pharDir = dirname(dirname(dirname(__FILE__))).DS.'Vendor'.DS.'Composer'.DS;
 		}
 	}
 	
@@ -21,8 +21,24 @@ class ComposerShell extends AppShell {
 		$pharURL = 'http://getcomposer.org/composer.phar';
 		if (Configure::read('Composer.phar_url') !== null) $pharURL = Configure::read('Composer.phar_url');
 		
-		if (!is_writable($pharDir)){
-			$this->error("$pharDir is not writable.");
+		if (!is_writable($this->pharDir)){
+			$this->error("$this->pharDir is not writable.");
 		}
+		
+		$this->out('<info>Setting up Composer</info>');
+		$this->out("Downloading from $pharURL...");
+		
+		$content = file_get_contents($pharURL);
+		if ($content === false){
+			$this->error("Download failed");
+		}
+		
+		$save = file_put_contents($this->pharDir.'composer.phar', $content);
+		
+		if ($save === false){
+			$this->error("Unable to save to {$this->pharDir}composer.phar.");
+		}
+		
+		$this->out("Composer.phar installed and saved successfully.");
 	}
 }
