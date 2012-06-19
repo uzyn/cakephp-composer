2da15e39cd68541c4ce94ad79ec46419fea8c4cc
U-Zyn Chua <chua@uzyn.com>
2012-06-20 00:04:56 +0800

    checkComposerPhar() as private function

--- Console/Command/ComposerShell.php
+++ Console/Command/ComposerShell.php
@@ -12,23 +12,7 @@ class ComposerShell extends AppShell {
 			else $this->pharDir = dirname(dirname(dirname(__FILE__))).DS.'Vendor'.DS.'Composer'.DS;
 		}
 		
-		/**
-		 * Check if composer.phar is available
-		 */
-		$version = @exec("php {$this->pharDir}composer.phar --version");
-		
-		if (stripos($version, 'Composer') === false || stripos($version, 'version') === false) {
-			$this->out('<warning>Composer is not installed.</warning>');
-			$setup = $this->in('Would you like Composer to be set up automatically?', array('y', 'n'), 'y');
-			
-			if ($setup != 'y') {
-				$this->error("Terminating. You may overwrite the location of composer.phar by defining 'Composer.phar_dir' configuration.");
-			}
-			else {
-				$this->setup();
-			}
-		}
-		
+		$this->_checkComposerPhar();
 		$this->_checkComposerJSON();
 	}
 	
@@ -106,6 +90,26 @@ class ComposerShell extends AppShell {
 		}
 		
 		return $results;
+	}
+	
+	/**
+	 * Check if composer.phar is available
+	 * Offer to install if it isn't available
+	 */
+	private function _checkComposerPhar(){
+		$version = @exec("php {$this->pharDir}composer.phar --version");
+		
+		if (stripos($version, 'Composer') === false || stripos($version, 'version') === false) {
+			$this->out('<warning>Composer is not installed.</warning>');
+			$setup = $this->in('Would you like Composer to be set up automatically?', array('y', 'n'), 'y');
+			
+			if ($setup != 'y') {
+				$this->error("Terminating. You may overwrite the location of composer.phar by defining 'Composer.phar_dir' configuration.");
+			}
+			else {
+				$this->setup();
+			}
+		}
 	}
 	
 	/**
