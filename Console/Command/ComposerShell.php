84e96cf48cf9bdc99db908d208f713df469858b2
U-Zyn Chua <chua@uzyn.com>
2012-06-19 20:57:38 +0800

    accepts composer options

--- Console/Command/ComposerShell.php
+++ Console/Command/ComposerShell.php
@@ -6,8 +6,8 @@ class ComposerShell extends AppShell {
 	 * Initialize ComposerShell
 	 * also checks if Composer is installed and offer auto installation option.
 	 */
-	public function initialize(){
-		if (empty($this->pharDir)){
+	public function initialize() {
+		if (empty($this->pharDir)) {
 			if (Configure::read('Composer.phar_dir') !== null) $this->pharDir = Configure::read('Composer.phar_dir');
 			else $this->pharDir = dirname(dirname(dirname(__FILE__))).DS.'Vendor'.DS.'Composer'.DS;
 		}
@@ -17,38 +17,37 @@ class ComposerShell extends AppShell {
 		 */
 		$version = @exec("php {$this->pharDir}composer.phar --version");
 		
-		if (stripos($version, 'Composer') === false || stripos($version, 'version') === false){
+		if (stripos($version, 'Composer') === false || stripos($version, 'version') === false) {
 			$this->out('<warning>Composer is not installed.</warning>');
 			$setup = $this->in('Would you like Composer to be set up automatically?', array('y', 'n'), 'y');
 			
-			if ($setup != 'y'){
+			if ($setup != 'y') {
 				$this->error("Terminating. You may overwrite the location of composer.phar by defining 'Composer.phar_dir' configuration.");
 			}
-			else{
+			else {
 				$this->setup();
 			}
 		}
 		
 	}
 	
-	public function startup(){
+	public function startup() {
 		$this->out("<info>Composer plugin for CakePHP</info> by U-Zyn Chua.", 2);
 	}
 	
 	public function main() {
-		
-		$this->out("php {$this->pharDir}composer.phar ".implode(" ", $this->args));
+		passthru("php {$this->pharDir}composer.phar ".implode(" ", $this->args)." ".self::_optionsToString($this->params));
 	}
 	
 	/**
 	 * Grabs the latest composer.phar from http://getcomposer.org/composer.phar
 	 * Changeable at CakePHP configuration: Composer.phar_url
 	 */
-	public function setup(){
+	public function setup() {
 		$pharURL = 'http://getcomposer.org/composer.phar';
 		if (Configure::read('Composer.phar_url') !== null) $pharURL = Configure::read('Composer.phar_url');
 		
-		if (!is_writable($this->pharDir)){
+		if (!is_writable($this->pharDir)) {
 			$this->error("$this->pharDir is not writable.");
 		}
 		
@@ -56,13 +55,13 @@ class ComposerShell extends AppShell {
 		$this->out("Downloading composer.phar from $pharURL...");
 		
 		$content = file_get_contents($pharURL);
-		if ($content === false){
+		if ($content === false) {
 			$this->error("Download failed");
 		}
 		
 		$save = file_put_contents($this->pharDir.'composer.phar', $content);
 		
-		if ($save === false){
+		if ($save === false) {
 			$this->error("Unable to save to {$this->pharDir}composer.phar.");
 		}
 		
@@ -87,5 +86,24 @@ class ComposerShell extends AppShell {
 		));
 		
 		return $parser;
+	}
+	
+	/**
+	 * Convert options to string
+	 * 
+	 * @param array $options Options array
+	 * @return string Results
+	 */
+	private static function _optionsToString($options) {
+		if (empty($options) || !is_array($options)) return '';
+		
+		$results = '';
+		foreach ($options as $option => $value) {
+			if (strlen($results) > 0) $results .= ' ';
+			if (empty($value)) $results .= "--$option";
+			else $results .= "--$option=$value";
+		}
+		
+		return $results;
 	}
 }
